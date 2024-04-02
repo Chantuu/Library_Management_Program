690376607767ba6bb9b97cd98f780cd0d1143441
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-04-03 02:02:30 +0400

    Create GenericRepository.cs
     Created new generic repository to write common functionality and share across different
    repositories without code duplication and more maintainability.

---
+++ LibraryManagement.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using LibraryManagement.Infrastructure.Entities;
+
+namespace LibraryManagement.Infrastructure.Repositories;
+
+public class GenericRepository<T> : IRepository<T>, IDisposable
+where T : class
+{
+    readonly LibraryContext _context;
+    
+    public GenericRepository()
+    {
+        _context = FactoryPattern.CreateLibraryContext();
+    }
+    
+    public virtual void Add(T entity)
+    {
+        _context.Add(entity);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _context.Update(entity);
+    }
+
+    public virtual T? Get(int id)
+    {
+        return _context.Find<T>(id);
+    }
+
+    public virtual T? First(Func<T, bool> match)
+    {
+        return _context.Set<T>().FirstOrDefault(match);
+    }
+
+    public virtual IEnumerable<T> Find(Func<T, bool> predicate)
+    {
+        return _context.Set<T>().Where(predicate);
+    }
+
+    public virtual IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>();
+    }
+
+    public virtual void Delete(T entity)
+    {
+        _context.Remove(entity);
+    }
+
+    public virtual void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+    
+    public virtual void Dispose()
+    {
+        _context.Dispose();
+    }
+}
