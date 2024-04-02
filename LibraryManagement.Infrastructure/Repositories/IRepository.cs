ed21bfd5e59a8782f4f92cec17d0d777399a5420
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-04-03 01:29:34 +0400

    Changed Method Signatures in IRepository.cs
     Changed return types to nullable types for Get and First methods, because implementation of
    EF Core returns nullable results.

--- LibraryManagement.Infrastructure/Repositories/IRepository.cs
+++ LibraryManagement.Infrastructure/Repositories/IRepository.cs
@@ -4,8 +4,8 @@ public interface IRepository<T>
 {
     void Add(T entity);
     void Update(T entity);
-    T Get(int id);
-    T First(Func<T, bool> match);
+    T? Get(int id);
+    T? First(Func<T, bool> match);
     IEnumerable<T> Find(Func<T, bool> predicate);
     IEnumerable<T> GetAll();
     void Delete(T entity);
