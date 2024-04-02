b98c2b13b2a39c690164b4af26c1185afb6c2fcc
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-04-03 00:50:01 +0400

    Reverse Engineer Database
     Reverse engineered database.db into entities and created DbContext for communicating
    with the database using EF Core scaffolding.

---
+++ LibraryManagement.Infrastructure/Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryManagement.Infrastructure.Entities;
+
+public partial class Book
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? AuthorId { get; set; }
+
+    public int? GenreId { get; set; }
+
+    public string? PublishDate { get; set; }
+
+    public double? Rating { get; set; }
+
+    public virtual Author? Author { get; set; }
+
+    public virtual Genre? Genre { get; set; }
+}
---
+++ LibraryManagement.Infrastructure/Entities/LibraryContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Infrastructure.Entities;
+
+public partial class LibraryContext : DbContext
+{
+    public LibraryContext()
+    {
+    }
+
+    public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=\"D:\\Projects\\Portfolio_Projects\\Library_Management_Program\\LibraryManagement.Infrastructure\\Database\\database.db\"");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("ID");
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("ID");
+            entity.Property(e => e.AuthorId).HasColumnName("Author_ID");
+            entity.Property(e => e.GenreId).HasColumnName("Genre_ID");
+            entity.Property(e => e.PublishDate).HasColumnName("Publish_Date");
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasOne(d => d.Genre).WithMany(p => p.Books)
+                .HasForeignKey(d => d.GenreId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Genre>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("ID");
+            entity.Property(e => e.Genre1).HasColumnName("Genre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
