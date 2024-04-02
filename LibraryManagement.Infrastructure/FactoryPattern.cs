514538c14425cd0069124f521c44a3505a47a263
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-04-03 01:32:16 +0400

    Create FactoryPattern Class
     Created FactoryPattern.cs to provide easy and maintainable solution
    for creating new contexts in the Repository classes without exposing
    them to the application layer.

---
+++ LibraryManagement.Infrastructure/FactoryPattern.cs
@@ -0,0 +1,11 @@
+﻿using LibraryManagement.Infrastructure.Entities;
+
+namespace LibraryManagement.Infrastructure;
+
+public static class FactoryPattern
+{
+    public static LibraryContext CreateLibraryContext()
+    {
+        return new LibraryContext();
+    }
+}
