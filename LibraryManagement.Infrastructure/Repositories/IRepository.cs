79b76cb9946233363c63ad1d975b6145aea3d314
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-04-03 01:11:22 +0400

    Create IRepository.cs
     Created interface for repositories, which lists all required methods to be implemented.

---
+++ LibraryManagement.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement.Infrastructure.Repositories;
+
+public interface IRepository<T>
+{
+    void Add(T entity);
+    void Update(T entity);
+    T Get(int id);
+    T First(Func<T, bool> match);
+    IEnumerable<T> Find(Func<T, bool> predicate);
+    IEnumerable<T> GetAll();
+    void Delete(T entity);
+    void SaveChanges();
+}
