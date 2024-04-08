c30ab08f374b59fb1ee4ed0307f234314cb169ee
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2024-04-08 14:06:25 +0400

    Add Displaying Menu Title Method

---
+++ LibraryManagement.Application/Core/Processes.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryManagement.Application.Core
+{
+    internal static class Processes
+    {
+        private static string _incorrectInputError = "The input is incorrect. Please try again!";
+
+        public static void DisplayMenuTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
